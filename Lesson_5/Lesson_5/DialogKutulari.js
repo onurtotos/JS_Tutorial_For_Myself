bb90e813297f6621d63ac7dae917a408206d12fd
onurtotos <onurtotos@gmail.com>
2023-06-15 16:38:11 +0300

    JS_Lesson_5

---
+++ Lesson_5/Lesson_5/DialogKutulari.js
@@ -0,0 +1,31 @@
+﻿
+
+/*
+                                        /   /console.log(window);
+     -Dialog Kutulari
+
+    **Bunlar birer fonksiyondur
+     *alert
+     *prompt
+     *confirm
+*/
+
+
+
+
+//Sayfanin ustunde onay almak veya mesaj vermek gibi islemler icin kullanilir
+alert("Lutfen OK butonuna basiniz!");
+
+
+//Kullanicidan deger alabilmek icin kullanilir. Ve her zaman string doner.
+//prompt("Isminizi Giriniz:");
+let isim = prompt("Isminizi Giriniz:");
+console.log(isim);
+let yas = prompt("Yasinizi Giriniz:");
+console.log(yas);
+
+
+//Boolean dondurur.
+let sonuc = confirm("Silmek istediginizi emin misiniz?");
+console.log(sonuc); // Kullanici OK a basarsa True; Cancel derse False.
+
---
+++ Lesson_5/Lesson_5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lesson_5.Global" Language="C#" %>
