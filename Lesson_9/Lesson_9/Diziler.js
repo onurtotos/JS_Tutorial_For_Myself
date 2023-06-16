b39ab6a2d7ea489dd595b56b0691f2be03e4855c
onurtotos <onurtotos@gmail.com>
2023-06-16 16:22:16 +0300

    JS_Lesson_9

---
+++ Lesson_9/Lesson_9/Diziler.js
@@ -0,0 +1,96 @@
+﻿
+
+                //Arrays (Diziler)
+
+
+
+
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(numbers[11]);
+
+numbers[9] = "Fener";
+console.log(numbers);
+
+
+let names = ["John", "Marry", "Ahmet", "Ayse"];
+
+
+let mixed = [0, 2.5, true, null, undefined, "Cimbom"];
+
+
+
+/*          FOREACH DONGUSU           */
+
+names.forEach(function (isim) {
+    console.log(isim);
+});
+
+let sayac = 0;
+while (sayac < names.length) {
+    console.log(names[sayac]);
+    sayac++;
+}
+
+/*                      Dizi Methodlari              */
+
+
+
+
+/*
+
+    push: Dizinin sonuna eleman ekler, ayrica dizinin uzunlugunu doner
+    unshift: dizinin basina eleman ekler, eleman sayisini geriye doner.
+
+    pop: Dizinin sonundan eleman siler, eleman sayisini doner.
+    shift: Dizinin basindan eleman siler, eleman sayisini doner.
+
+    splice(index,incdex): eleman eklemek ve silmek icin kullanilir.
+
+    toString : diziyi stringe cevirir.
+    join: Diziyi stringe cevirir. Farki ise araya eleman ekleyebiliriz.
+
+    concat: Dizileri birlestirmek icin kullanilir.
+    slice: Diziyi istenilen yerden bolup yeni bir dizi olusturur.
+    length: dizinin uzunlugunu verir.
+    reverse: dizinin elemanlarini ters cevirir.
+    split: belirli bir ifadeye gore bolup diziye cevirir.
+    indexOf: elemanin index numarasini icerir.
+    includes: verilen elemani iceriyor mu ona bakar.
+
+*/
+
+
+
+let cars = ["Bmw", "Merso", "Ferrari", "Renault", "Porsche"];
+
+//PUSH
+console.log(cars);
+cars.push("Sercedes");
+console.log(cars);
+
+//UNSHIFT
+cars.unshift("Merhaba");
+console.log(cars);
+
+//POP
+cars.pop();
+console.log(cars);
+
+//SHIFT
+cars.shift();
+console.log(cars);
+
+//SPLICE
+cars.splice(2, 0, "Fener");
+console.log(cars);
+
+//TOSTRING
+let arabalar=cars.toString();
+console.log(arabalar);
+
+//JOIN
+arabalar=cars.join("-");
+console.log(arabalar);
+
+//CONCAT
---
+++ Lesson_9/Lesson_9/urunAramaUygulamasi.js
@@ -0,0 +1,59 @@
+﻿
+
+let urun1 = {
+    isim: "Acer Swift",
+    kategori: "Teknoloji",
+    fiyat:7000
+}
+let urun2 = {
+    isim: "Acer Nitro 5",
+    kategori: "Teknoloji",
+    fiyat: 17000
+}
+let urun3 = {
+    isim: "Acer Gaming",
+    kategori: "Teknoloji",
+    fiyat: 14000
+}
+let urun4 = {
+    isim: "Lenovo V15",
+    kategori: "Teknoloji",
+    fiyat: 11000
+}
+let urun5 = {
+    isim: "Lenovo V14",
+    kategori: "Teknoloji",
+    fiyat: 12000
+}
+let urun6 = {
+    isim: "Lenovo V13",
+    kategori: "Teknoloji",
+    fiyat: 14000
+}
+
+let urunler = [urun1, urun2, urun3, urun4, urun5, urun6]
+/*console.log(urunler);*/
+
+let filtreliUrunler = [];
+let kullaniciUrunIsmi = prompt("Bir urun ismi giriniz:");
+
+function getUrunler(urunler) {
+    urunler.forEach(function (urun) {
+        if (urun.isim.toUpperCase().includes(kullaniciUrunIsmi.toUpperCase(), 0)) {
+            filtreliUrunler.push(urun)
+        }
+      
+    });
+    
+}
+
+function printUrunler(urunler) {
+    urunler.forEach(function (urun) {
+        console.log("----------------------------------------");
+        console.log("|" + urun.isim + "|" + urun.fiyat + "|" + urun.kategori);
+        console.log("----------------------------------------");
+    });
+}
+
+getUrunler(urunler);
+printUrunler(filtreliUrunler);
