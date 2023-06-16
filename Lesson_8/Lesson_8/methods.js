62ae30cbe7724c1a6def73f481855ba2808bc3f2
onurtotos <onurtotos@gmail.com>
2023-06-16 16:21:33 +0300

    JS_Lesson_8

---
+++ Lesson_8/Lesson_8/methods.js
@@ -0,0 +1,92 @@
+﻿
+
+
+/*                  METHODLAR       
+
+
+    1) Parametre almayan ve geriye deger dondurmeyen methot
+
+*/
+
+/*
+    *function methodName () {
+
+    //codeblock
+    }
+    *
+
+
+function yazdir() {
+    console.log("Onur");
+}
+
+yazdir();
+
+function topla() {
+    console.log(8 + 9);
+}
+
+topla();
+*/
+/* ---------------------------------------------------------------------------------------*/
+
+
+/*                  METHODLAR       
+
+
+    2) Parametre alan ve geriye deger dondurmeyen methot
+
+ */
+
+/*
+function yazdir(isim,soyisim) {
+    console.log(isim,soyisim);
+}
+
+yazdir("fener","bahce");
+
+
+function topla(sayi1, sayi2) {
+   
+    console.log(sayi1+ sayi2);
+}
+topla(9, 8);
+
+let yas = Number(prompt("Yasinizi Giriniz:"));
+
+function kontrolEt(yas) {
+    if (yas>18) {
+        alert("Ehliyeti alabilirsiniz!");
+    }
+    else {
+        alert("Ehliyet icin " + Math.abs(yas - 18) + " yil beklemeniz gerekli!", );
+    }
+}
+
+kontrolEt(yas);
+
+*/
+
+
+/*                  METHODLAR       
+
+
+    3) Geriye deger donduren ve parametre alan methot
+
+*/
+
+
+let donenDeger = Cube(8);
+console.log(donenDeger);
+function Cube(sayi) {
+    let sonuc = sayi * sayi * sayi;
+    return sonuc;
+}
+
+
+function kareAl(sayi) {
+    let sonuc = sayi * sayi;
+    console.log(sonuc);
+}
+
+kareAl(donenDeger);
---
+++ Lesson_8/Lesson_8/ornekler.js
@@ -0,0 +1,111 @@
+﻿
+
+/*
+    Kelime sayis bulma uygulamasi // return methodu
+
+    Metin isteyecegiz.
+    Harf isteyecegiz.
+    Sonra da girilen metindeki, girilen harf sayisinin kac tane oldugunu yazdiracagiz.
+
+*/
+/*
+let metin = prompt("Lutfen metini giriniz:");
+let harf = prompt("Lutfen harf giriniz:");
+
+function harfSayisi(harf) {
+    let toplam = 0;
+    for (let i = 0; i < metin.length; i++) {
+        if (metin.charAt(i) === harf) {
+            toplam += 1;
+        }
+    }
+    return toplam;
+}
+yeniSatir = "\r\n";
+let sonuc = harfSayisi(harf);
+alert("Harf sayisi: " + sonuc + yeniSatir + "Girilen metin: " + metin + yeniSatir + "Girilen harf: " + harf)
+*/
+
+
+
+/*          
+        Mukemmel Sayi Uygulamasi
+  
+        Mukemmel Sayi = Kendisi haric bolenlerinin toplami ; kendisine esitse Mukemmel Sayidir.
+
+*/
+/*
+let number = prompt("Lutfen sayi giriniz: ");
+
+function perfectNumber(number) {
+
+    let toplam = 0;
+    for (let i = 1; i < number; i++) {
+        if (number%i ==0) {
+            toplam = toplam+i;
+        }
+    }
+    if (toplam == number) {
+        alert("Girilen sayi mukemmel bir sayidir! " + number)
+    } else {
+        alert("Girilen sayi mukemmel bir sayi degildir! " + number)
+    }
+    return number;
+}
+
+    let sonuc1 = perfectNumber(number);
+ */
+
+/* Decimal to Binary */
+
+/*
+
+let number = prompt("Lutfen sayi giriniz:");
+
+function convertDecimalToBinary(number) {
+    let binary = "";
+    while (true) {
+        binary += String(number%2);
+        number = Math.floor(number/2);
+        if (number == 1) {
+            binary += 1;
+            break;
+        }
+    }
+    let result = reverse(binary);
+    alert("Sonuc: " + result);
+    return number;
+}
+
+function reverse(binary) {
+    let reverseBinary = "";
+    for (let i = binary.length-1; i >=0; i--) {
+        reverseBinary += binary.charAt(i);
+    }
+    return reverseBinary;
+}
+
+
+    let sonuc2 = convertDecimalToBinary(number);
+
+ */
+
+/*
+    
+        Binary to Decimal
+
+*/
+
+let binary =prompt("Binary sayinizi giriniz:");
+
+function binaryToDecimal(binary) {
+    let toplam = 0;
+    let ust = 0;
+    for (let i = binary.length-1; i >= 0; i--) {
+        toplam += Number(binary.charAt(i)) * (Math.pow(2, ust));
+        ust++;
+    }
+    alert("Sonuc: " + toplam);
+}
+binaryToDecimal(binary);
+
